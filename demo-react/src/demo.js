3f7532cf1beffa511d38e712f6e9cb0fb486dc6f
Christopher Polito <chris@mwclass.io>
2022-06-08 12:58:42 -0400

    touch testing

--- demo-react/src/demo.js
+++ demo-react/src/demo.js
@@ -1,8 +1,7 @@
 import mcaccel from '@methodswithclass/accelerometer';
 
-
 const load = () => {
-  var demo = false; // test the accelerometer engine with mouse data instead of device accelerometer data
+  var demo = true; // test the accelerometer engine with mouse data instead of device accelerometer data
   //parameters for numerical integration process and general motion behavior
   var params = {
     interval: 2, //how often the accelerometer data is sampled in milliseconds
@@ -71,7 +70,6 @@ const load = () => {
 
   //start updating position of DOM element based on accelerometer data
   accel.start();
-
 };
 
 export default {
